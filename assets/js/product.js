a29988f3ae24649e19972241cb74200e171306e3
KatWest <katherine.west@bellevuecollege.edu>
2025-03-06 16:25:53 -0800

    work on browse page

--- assets/js/product.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {    
-    const urlParams = new URLSearchParams(window.location.search);
-    document.getElementById('product-name').innerHTML = urlParams.get('pid');
-}
