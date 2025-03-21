b559775e3f01268b3f00683c958a9374b5f380db
pdottroendle <pdottroendle@gmail.com>
2025-03-21 16:25:48 -0700

    Update cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,4 +1,3 @@
-
 // Get the json of products
 
 // Store the product array in session storage
@@ -17,18 +16,40 @@ function addToCart() {
                     cart.push(selectedProduct);
                     sessionStorage.setItem('cart', JSON.stringify(cart));
                     location.reload();
+                    console.log("added to cart");
                 }
                 else document.getElementById("product-info").innerHTML = `<p>Product not found.</p>`;
             }
         })
-    /*// Retrieve the existing cart from session storage, or initialize an empty array if it doesn't exist
+}
+
+function quickAddToCart(productId) {
+    fetch('../json/products.json')
+        .then(response => response.json())
+        .then(products => {
+            let cart = JSON.parse(sessionStorage.getItem('cart')) || [];
+            var selectedProduct = products.find(p => p.pid == productId);
+            console.log(selectedProduct);
+            if (selectedProduct != undefined) {
+                cart.push(selectedProduct);
+                sessionStorage.setItem('cart', JSON.stringify(cart));
+                console.log("added to cart");
+                location.reload();
+            }
+        })
+}
+
+// Function to remove a product from the cart
+function removeFromCart(index) {
     let cart = JSON.parse(sessionStorage.getItem('cart')) || [];
-
-    // Append the new item to the cart
-    cart.push(selectedPerson);
-
-    // Store the updated cart back in session storage
-    sessionStorage.setItem('cart', JSON.stringify(cart));*/
+    if (index > -1 && index < cart.length) {
+        cart.splice(index, 1);
+        sessionStorage.setItem('cart', JSON.stringify(cart));
+        console.log("removed from cart");
+        location.reload();
+    } else {
+        console.log("Invalid index");
+    }
 }
 
 // Function to display the selected product information
@@ -37,7 +58,7 @@ function displayCart() {
         const cartContent = JSON.parse(sessionStorage.getItem('cart'));
         personInfoDiv.innerHTML = ``;
 
-        cartContent.forEach(item => {
+        cartContent.forEach((item, index) => {
             if (item != null) {
                 personInfoDiv.innerHTML += `
                 <div class="product-card">
@@ -46,35 +67,10 @@ function displayCart() {
                     <p>Description: ${item.description}</p>
                     <img src="${item.image}" alt="Product Image">
                     <p>PID: ${item.pid}</p>
+                    <button onclick="removeFromCart(${index})">Remove</button>
                 </div>
                 `
             }
         });
-
-        /*
-        // Retrieve the product array from session storage
-        //const person = JSON.parse(sessionStorage.getItem('person'));
-        
-        // Get the value from the input field
-        //const fname = document.getElementById('fname0').value;
-        
-        // Find the product that matches the input value
-        //const selectedPerson = person.find(p => p.pName === fname);
-        
-        // Add the selected product to the cart
-        //addToCart(selectedPerson);
-
-        // Display the product information if found, otherwise show a "Product not found" message
-        if (selectedPerson) {
-            personInfoDiv.innerHTML = `
-                <p>PID: ${selectedPerson.pid}</p>
-                <p>Name: ${selectedPerson.pName}</p>
-                <p>Price: ${selectedPerson.price}</p>
-                <p>Description: ${selectedPerson.description}</p>
-                <p>Image: <img src="${selectedPerson.image}" alt="Product Image"></p>
-            `;
-        } else {
-            personInfoDiv.innerHTML = `<p>Product not found.</p>`;
-        } */
     }    
 }
